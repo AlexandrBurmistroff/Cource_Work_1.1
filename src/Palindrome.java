653da207af7b3d0ea45776965decee3d25f343ef
Alexandr.Burmistroff <albook366314@gmail.com>
2023-05-19 13:01:11 +0200

    Курсовая 1.1: Задача на билет №2 - палиндром. На проверку

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,6 +1,22 @@
 public class Palindrome {
     public static void main(String[] args) {
+        System.out.println(isPalindrome("мама"));
+        System.out.println(isPalindrome("КАБАН"));
+        System.out.println(isPalindrome("КАБАК"));
+        System.out.println(isPalindrome("поТоп"));
 
+    }
 
+    public static boolean isPalindrome(String text) {
+        String string = text.toUpperCase();
+        StringBuilder stringBuilder = new StringBuilder(string).reverse();
+        boolean answer = true;
+        for (int count = 0; count < string.length(); count++) {
+            if (string.charAt(count) != stringBuilder.charAt(count)) {
+                answer = false;
+                break;
+            }
+        }
+        return answer;
     }
 }
